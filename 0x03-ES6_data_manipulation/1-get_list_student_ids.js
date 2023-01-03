7e931b1134a0795fa68649c0405c6335757b0b23
auntyemman <1mole.coco@gmail,com>
2023-01-03 15:33:30 +0000

    Initial commit

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -3,7 +3,7 @@ export default function getListStudentIds(arg){
 	 const arrObj = [
 		 { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
 		 { id: 2, firstName: 'James', location: 'Columbia' },
-		 { id: 5, firstName: 'Serena', location: 'San Francisco' },
+		 { id: 5, firstName: 'Serena', location: 'San Francisco' }
 	 ];
         const mapArrObj = arrObj.map((property) => property.id);
 	if (Array.isArray(arg)){
