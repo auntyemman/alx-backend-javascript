18e068683ededfd25867140387f647aabecbbf6f
auntyemman <1mole.coco@gmail,com>
2023-01-03 17:20:25 +0000

    Initial commit

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,13 +1,13 @@
 /*-----Using Map method in a function-----*/
 export default function getListStudentIds(arg){
-	 const arrObj = [
-		 { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-		 { id: 2, firstName: 'James', location: 'Columbia' },
-		 { id: 5, firstName: 'Serena', location: 'San Francisco' }
-	 ];
-        const mapArrObj = arrObj.map((property) => property.id);
-	if (Array.isArray(arg)){
-                return mapArrObj;
-        }
-        return [];
+  const arrObj = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ];
+  const mapArrObj = arrObj.map((property) => property.id);
+  if (Array.isArray(arg)){
+    return mapArrObj;
+  }
+  return [];
 }
