4b17d1519e269a87472c337a824b1b8f84e629ee
auntyemman <1mole.coco@gmail.com>
2023-02-25 00:39:49 +0100

    Initial commit

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -10,7 +10,6 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 module.exports = app;
