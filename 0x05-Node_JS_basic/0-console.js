0ce20014759219c412f84053448853ce3f42795c
auntyemman <1mole.coco@gmail.com>
2023-02-24 22:25:29 +0100

    Initial commit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,7 +1,7 @@
 // A function that prints out some argument in STDOUT
 
-function displayMessage (message) {
-    process.stdout.write(message + '\n');
-};
+function displayMessage(message) {
+  process.stdout.write(`${message}\n`);
+}
 
 module.exports = displayMessage;
